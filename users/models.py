e0fa92e8a5a23c66394c8c68061e24b7e51b392e
Jaeyoung <tjy0711@naver.com>
2023-10-02 23:23:05 +0900

    initial

---
+++ users/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    cust_id = models.IntegerField(max_length=128, null=True)
+    name = models.CharField(max_length=128)
+    nickname = models.CharField(max_length=128, default="덕우")
+    phone = models.CharField(max_length=128)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
