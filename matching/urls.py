8ceee361a493b7204e694614609262b9fea9beab
Jaeyoung <tjy0711@naver.com>
2023-10-10 16:32:48 +0900

    매칭룸 연결

--- matching/urls.py
+++ matching/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'matching'
+
 urlpatterns = [
     path('create_matching_room/', views.create_matching_room, name='create_matching_room'),
     path('list_matching_rooms/', views.list_matching_rooms, name='list_matching_rooms'),
