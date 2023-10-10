15ff596de00abc6143071434bb8d5043add0fd44
Jaeyoung <tjy0711@naver.com>
2023-10-10 18:30:35 +0900

    선물메시지 입력 추가

--- gift/models.py
+++ gift/models.py
@@ -5,6 +5,7 @@ class Gift(models.Model):
     giver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='gifts_given', null=True)  # 선물한 사람 id
     taker = models.CharField(max_length=255)  # 선물 받은 사람 전화번호
     check = models.CharField(max_length=1, blank=True, null=True)
+    message = models.TextField(blank=True, null=True) #메시지
     create_dt = models.DateTimeField('CREATE DT', auto_now_add=True)
     update_dt = models.DateTimeField('UPDATE DT', auto_now=True)
 
