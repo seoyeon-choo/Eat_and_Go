3415b4a1bbb3231e09a62b7b347b43297bb79335
jiyeong <jyfullsun42@gmail.com>
2023-10-11 22:03:21 +0900

    (리스트수정)

--- list/urls.py
+++ list/urls.py
@@ -5,5 +5,8 @@ app_name = 'list'
 
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
+     path('review/', views.review, name='review'),
     path('<int:id>/<slug:slug>/', views.menu_detail, name='menu_detail'),
+    path('write_review/', views.write_review, name='write_review')
+
 ]
--- list/views.py
+++ list/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Menu
 from cart.forms import AddProductForm
+from django.shortcuts import render
+
 
 def menu_list(request):
     menus = Menu.objects.filter(available_display=True)
     return render(request, 'list/buy_list.html', {'menus': menus})
 
 def menu_detail(request, id, slug):
-    menu = get_object_or_404(Menu, id=id, slug=slug, available_display=True)
     add_to_cart = AddProductForm(initial={'quantity': 1})
-    return render(request, 'list/buy_detail.html', {'menu': menu, 'add_to_cart': add_to_cart})
+    return render(request, 'list/buy_detail.html')
 
 def menu_list(request):
     return render(request, 'list/buy_list.html')
+def review(request):
+
+    return render(request, 'list/review.html')
+
+
+def write_review(request):
+    
+    return render(request, 'list/write_review.html')
 
 
 # def success_view(request): #html을 보여주는 함수(여기다가 html 걸면 될듯~~)
